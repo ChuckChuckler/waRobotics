d270b5312d72419d3eb8d430f0af1b272b8048cf
ChuckChuckler <randompeoplekid@gmail.com>
2025-07-02 19:39:28 -0400

    Info added to summer camp/join pages. Started work on outreach page. Visuals on back burner for now, not priority

--- otherScripts.js
+++ otherScripts.js
@@ -1,3 +1,38 @@
+const SEASONS = [
+    document.getElementsByClassName("intoTheDeep")[0],
+    document.getElementsByClassName("centerStage")[0],
+    document.getElementsByClassName("powerplay")[0],
+    document.getElementsByClassName("freightFrenzy")[0],
+    document.getElementsByClassName("ultimateGoal")[0],
+    document.getElementsByClassName("skystone")[0],
+    document.getElementsByClassName("roverRuckus")[0],
+    document.getElementsByClassName("relicRecovery")[0]
+];
+
+let seasonIndex = 0;
+
+function next(){
+    seasonIndex+=1;
+    if(seasonIndex==SEASONS.length){
+        seasonIndex=0;
+        SEASONS[SEASONS.length-1].style.display = "none";
+    }else{
+        SEASONS[seasonIndex-1].style.display = "none";
+    }
+    SEASONS[seasonIndex].style.display = "flex";
+}
+
+function prev(){
+    seasonIndex-=1;
+    if(seasonIndex==-1){
+        seasonIndex = SEASONS.length-1;
+        SEASONS[0].style.display = "none";
+    }else{
+        SEASONS[seasonIndex+1].style.display = "none";
+    }
+    SEASONS[seasonIndex].style.display = "flex";
+}
+
 document.getElementById("gitIcon").addEventListener("mouseenter", function(){
   document.getElementById("gitInfo").innerText = "Github";
 });
